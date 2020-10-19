ad82b4aa0c089873c20f9c41d3e37044eb9c1e67
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-10-19 10:28:11 -0300

    Tailwind config

---
+++ tailwind.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0433FF",
+        dark_grey: "#2E2E2E",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
