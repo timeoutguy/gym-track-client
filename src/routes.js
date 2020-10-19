f875e567c41536cd55ca1eff407b9e2e60b21ebb
Xandowns <aphjunior@hotmail.com>
2020-10-19 16:25:16 -0300

    Rota de página inciial

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 // Importar páginas aqui
-// import Home from './pages/Home';
+import Home from './pages/Home';
 
 const Routes = () => {
-  return <Switch></Switch>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  );
 };
 
 export default Routes;
