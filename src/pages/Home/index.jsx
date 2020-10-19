eb2c1ac7ae7c312142a651fd1b47ba97f7dfbb5d
Xandowns <aphjunior@hotmail.com>
2020-10-19 15:54:43 -0300

    Mudanças requisitadas (texto, titulo e imagem)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,17 +5,17 @@ const Home = () => {
   return (
     <div className="p-4 lg:px-24 lg:py-16 lg:flex items-center ">
       <div className=" lg:w-1/2 pr-8">
-        <p className="text-3xl font-bold text-dark_grey">
+        <h1 className="text-3xl font-bold text-dark_grey">
           Facilite a forma como você treina
-        </p>
-        <p className="text-dark_grey text-lg ">
+        </h1>
+        <p className="text-dark_grey text-lg lg:text-2xl">
           Usando <b> Gym Track </b> você tem acesso as suas listar de exercicios
           para cada dia da semana e recebe mais informações sobre como eles
           atuam no seu corpo
         </p>
       </div>
-      <div className="lg:w-2/2">
-        <img src={hero} />
+      <div className="lg:ml-24">
+        <img src={hero} alt="Pessoa malhando" />
       </div>
     </div>
   );
