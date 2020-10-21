bcee89598f10c1bb8588e035bb8813a99091a853
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-10-21 12:43:55 -0300

    Alinhando tudo ao centro no mobile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,12 +5,12 @@ import Step from '../../components/Step';
 const Home = () => {
   return (
     <>
-      <div className="p-4 lg:px-24 lg:py-16 lg:flex items-center ">
+      <div className="p-4 flex flex-col items-center justify-center lg:px-24 lg:py-16 lg:flex-row">
         <div className=" lg:w-1/2 pr-8">
           <h1 className="text-3xl font-bold text-dark_grey">
             Facilite a forma como você treina
           </h1>
-          <p className="text-dark_grey text-lg lg:text-2xl">
+          <p className="text-dark_grey text-2xl">
             Usando <b> Gym Track </b> você tem acesso as suas listar de
             exercicios para cada dia da semana e recebe mais informações sobre
             como eles atuam no seu corpo
