93c379ffc1f50357508f0185573ee68518bdeaaa
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-10-26 14:12:52 -0300

    Rota da tela de login

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 // Importar páginas aqui
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
     </Switch>
   );
 };
