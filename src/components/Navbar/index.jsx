68be08022650a5adc983c04c14f85ffe1fc79110
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-10-26 14:14:01 -0300

    Componente de link adicionado a navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className="bg-primary h-20 px-2 lg:px-64 flex justify-between items-center font-bold">
-      <h1 className="text-3xl text-white"> Gym track </h1>
+      <Link to="/" className="text-3xl text-white">
+        Gym track
+      </Link>
       <div className="flex flex-row">
-        <p className="mr-4 bg-white py-2 px-4 rounded-full text-primary">
+        <Link
+          to="/login"
+          className="mr-4 bg-white py-2 px-4 rounded-full text-primary"
+        >
           Login
-        </p>
-        <p className="mr-4 bg-white py-2 px-4 rounded-full text-primary">
+        </Link>
+        <Link
+          to="/cadastro"
+          className="mr-4 bg-white py-2 px-4 rounded-full text-primary"
+        >
           Cadastro
-        </p>
+        </Link>
       </div>
     </div>
   );
