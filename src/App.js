e585806a1ccf3c990e2f9f8d708bf574ff5b2da1
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-10-26 15:02:45 -0300

    Adicionando footer no layout geral

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 import Routes from './routes';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes />
+      <Footer />
     </BrowserRouter>
   );
 }
