6f7ced2a78889ef3adf4b089303efe4302ead56d
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-10-28 15:13:55 -0300

    Adicionando select para o tipo de login e mudando as margens

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const initialState = {
+    type: 'academia',
+  };
+
+  const [type, setType] = useState(initialState.type);
+
   return (
     <div className="flex flex-col mx-auto justify-center p-4 lg:w-4/12 lg:mt-32">
       <h1 className="font-bold text-4xl text-dark_grey">Faça Login</h1>
       <h4 className="text-dark_grey font-light text-lg">
         Para visualizar o painel de ações é necessario fazer login
       </h4>
-      <label className="text-dark_grey text-2xl font-light mt-6">E-mail</label>
+      <label className="text-dark_grey text-2xl font-light mt-6">
+        Fazer login como?
+      </label>
+      <select
+        name="type"
+        id="type"
+        className="w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        value={type}
+        onChange={(e) => setType(e.target.value)}
+      >
+        <option value="academia">Academia</option>
+        <option value="instrutor">Instrutor</option>
+        <option value="aluno">Aluno</option>
+      </select>
+      <label className="text-dark_grey text-2xl font-light">E-mail</label>
       <input
         className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
         type="text"
@@ -15,7 +35,7 @@ const Login = () => {
         placeholder="Digite seu endereço de e-mail"
       />
 
-      <label for="password" className="text-dark_grey text-2xl font-light">
+      <label for="password" className="text-dark_grey text-2xl font-light mt-6">
         Senha
       </label>
       <input
