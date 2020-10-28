2813da94bc17c3aef2fa22861ec4bffa1f3dc4f9
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-10-28 10:44:38 -0300

    Definimos que apenas as academias fazem cadastro

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,35 +1,102 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import SignupGym from '../../components/SignupGym';
+import Cep from 'react-simple-cep-mask';
+import PhoneInput from 'react-phone-number-input/input';
 
 const Signup = () => {
-  const initialState = {
-    type: 'academia',
-  };
-
-  const [type, setType] = useState(initialState.type);
+  const [cep, setCep] = useState('');
+  const [phone, setPhone] = useState('');
 
   return (
-    <div className="flex flex-col mx-auto justify-center p-4 lg:w-4/12 lg:mt-32">
+    <div className="flex flex-col mx-auto justify-center p-4 lg:w-4/12 lg:mt-16">
       <h1 className="font-bold text-4xl text-dark_grey">Faça o seu cadastro</h1>
       <h4 className="text-dark_grey font-light text-lg">
         e tenha acesso a nossa plataforma
       </h4>
-      <label className="text-dark_grey text-2xl font-light mt-6">
-        Você quer se registrar como?
+      <label className="text-dark_grey text-2xl font-light mt-6">E-mail</label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="text"
+        name="email"
+        placeholder="Digite seu endereço de e-mail"
+      />
+      <label for="password" className="text-dark_grey text-2xl font-light mt-6">
+        Senha
       </label>
-      <select
-        name="type"
-        id="type"
-        className="w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        value={type}
-        onChange={(e) => setType(e.target.value)}
-      >
-        <option value="academia">Academia</option>
-        <option value="instrutor">Instrutor</option>
-        <option value="aluno">Aluno</option>
-      </select>
-      {type === 'academia' && <SignupGym />}
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="password"
+        name="password"
+        placeholder="Digite sua senha"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6">
+        Nome da academia
+      </label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="text"
+        name="name"
+        placeholder="Digite o nome da sua academia"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6">Bairro</label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="text"
+        name="neighborhood "
+        placeholder="Digite o bairro da academia"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6 w">Rua</label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="text"
+        name="street"
+        placeholder="Digite a rua da academia"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6 w">
+        Número
+      </label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="number"
+        name="number"
+        placeholder="Digite o bairro da academia"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6 w">
+        Complemento
+      </label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="text"
+        name="street"
+        placeholder="Digite um complemento da academia"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6 w">CEP</label>
+      <Cep
+        value={cep}
+        onChange={(cep) => setCep(cep)}
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        name="cep"
+        placeholder="Digite o cep da academia"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6 w">
+        Cidade
+      </label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="text"
+        name="city"
+        placeholder="Digite a cidade da academia"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6 w">
+        Telefone
+      </label>
+      <PhoneInput
+        country="BR"
+        value={phone}
+        onChange={setPhone}
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        placeholder="Digite o telefone da academia"
+      />
       <div className="flex flex-col justify-center items-center ">
         <button
           type="submit"
