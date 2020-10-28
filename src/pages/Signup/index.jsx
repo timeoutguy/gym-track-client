d3ccda93fa85318fb54d7b5fab2430a067e0310b
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-10-28 09:23:42 -0300

    Página de Signup e rota

---
+++ src/pages/Signup/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+  return (
+    <div className="flex flex-col mx-auto justify-center p-4 lg:w-4/12 lg:mt-32">
+      <h1 className="font-bold text-4xl text-dark_grey">Faça o seu cadastro</h1>
+      <h4 className="text-dark_grey font-light text-lg">
+        e tenha acesso a nossa plataforma
+      </h4>
+      <label className="text-dark_grey text-2xl font-light mt-6">E-mail</label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="text"
+        name="email"
+        placeholder="Digite seu endereço de e-mail"
+      />
+      <label className="text-dark_grey text-2xl font-light mt-6">
+        Confirme seu E-mail
+      </label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="text"
+        name="email"
+        placeholder="Digite seu endereço de e-mail"
+      />
+
+      <label for="password" className="text-dark_grey text-2xl font-light mt-6">
+        Senha
+      </label>
+      <input
+        className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+        type="password"
+        name="password"
+        placeholder="Digite sua senha"
+      />
+
+      <div className="flex flex-col justify-center items-center">
+        <button className="bg-primary mt-8 p-2 rounded-full text-white font-bold w-1/2">
+          Cadastrar
+        </button>
+        <p className="font-light mt-4">
+          Já possui uma conta? Faça login{' '}
+          <Link to="/cadastro" className="text-primary font-normal">
+            clicando aqui
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 // Importar páginas aqui
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
+      <Route path="/cadastro" component={Signup} />
     </Switch>
   );
 };
