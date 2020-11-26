a32c346bd6b47f76c0cac4f5c7c270e502ffe542
timeoutguy <marcusvyny92@gmail.com>
2020-11-26 15:31:39 -0300

    Melhorando o uso de espaço da tabela e adicionando os icones de exclusão e edição

--- src/components/Students/index.jsx
+++ src/components/Students/index.jsx
@@ -8,35 +8,31 @@ import { ReactComponent as Regua } from '../../assets/regua.svg';
 
 const Students = () => {
   return (
-    <div className=" flex flex-col md:flex-row border-t border-b p-4 ">
-      <p className="font-bold text-dark_grey mb-4 md:mb-0">Alexandre Hummel</p>
-      <div className="flex flex-row ">
-        <Zap className="h-12 w-12 md:pl-8 pb-5" />
+    <div className=" flex flex-col md:flex-row border-t p-4">
+      <p className="font-bold text-dark_grey md:w-3/6">
+        Alexandre Hummel Pereira Junior
+      </p>
+      <div className="flex flex-row md:w-2/6">
+        <Zap className="h-12 w-12 pb-5" />
         <p className="text-dark_grey md:pl-2">(xx)xxxxxxxxx</p>
       </div>
-      <div className="flex flex-row">
-        <Regua className="h-12 w-12 md:pl-8 pb-5" />
+      <div className="flex flex-row md:w-1/6">
+        <Regua className="h-12 w-12 pb-5" />
         <p className="text-dark_grey md:pl-2">175 cm</p>
       </div>
-      <div className="flex flex-row">
-        <Peso className="h-12 w-12 md:pl-8 pb-5" />
+      <div className="flex flex-row md:w-1/6">
+        <Peso className="h-12 w-12 pb-5" />
         <p className="text-dark_grey md:pl-2">110 kg</p>
       </div>
-      <div className="flex flex-row">
-        <Casa className="h-12 w-12 md:pl-8 pb-5" />
+      <div className="flex flex-row md:w-5/6">
+        <Casa className="h-12 w-12 pb-5" />
         <p className="text-dark_grey md:pl-2">
-          Rua Barbosa Ferraz, 272, Centro
+          Rua Barbosa Ferraz, 272, Centro, Cachoeira Paulista - 12630-000
         </p>
       </div>
-      <div className="flex flex-row md:flex-col h-12 w-12 md:pl-8 pb-5">
-        <div className="flex flex-row ml-3 mr-2">
-          <Delete className="h-6 w-6 mr-2 md:mr-0 md:h-12 md:w-12  " />
-          <p className="text-primary mr-2 md:pl-2">Editar</p>
-        </div>
-        <div className="flex flex-row">
-          <Edit className="h-6 w-6 mr-2 md:pl-8" />
-          <p className="text-red-700 md:pl-2">Excluir</p>
-        </div>
+      <div className="flex flex-row md:w-6/6">
+        <Delete className="h-6 w-6 fill-current text-red-600" />
+        <Edit className="h-6 w-6 fill-current ml-4 text-primary" />
       </div>
     </div>
   );
