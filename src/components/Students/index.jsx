b48a2007f9a343c3f34952fc9a399c0fb413d425
timeoutguy <marcusvyny92@gmail.com>
2020-11-30 13:48:45 -0300

    Estudantes na dashboard

--- src/components/Students/index.jsx
+++ src/components/Students/index.jsx
@@ -1,40 +1,237 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as Delete } from '../../assets/deletar-usuario.svg';
 import { ReactComponent as Edit } from '../../assets/editar.svg';
 import { ReactComponent as Zap } from '../../assets/zap.svg';
 import { ReactComponent as Casa } from '../../assets/casa.svg';
 import { ReactComponent as Peso } from '../../assets/peso.svg';
 import { ReactComponent as Regua } from '../../assets/regua.svg';
+import Modal from 'react-modal';
+import InputMask from 'react-input-mask';
 
-const Students = () => {
+const Students = ({
+  id,
+  name,
+  phone,
+  height,
+  weight,
+  street,
+  number,
+  neighborhood,
+  city,
+  zipcode,
+  sex,
+}) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [data, setData] = useState({
+    id,
+    name,
+    phone,
+    height,
+    weight,
+    street,
+    number,
+    neighborhood,
+    city,
+    zipcode,
+    sex,
+  });
+
+  const handleDelete = () => {
+    console.log(id);
+  };
+
+  const handleEdit = () => {
+    console.log(id);
+    setModalIsOpen(false);
+  };
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
   return (
-    <div className=" flex flex-col md:flex-row border-t p-4">
-      <p className="font-bold text-dark_grey md:w-3/6">
-        Alexandre Hummel Pereira Junior
-      </p>
-      <div className="flex flex-row md:w-2/6">
-        <Zap className="h-12 w-12 pb-5" />
-        <p className="text-dark_grey md:pl-2">(xx)xxxxxxxxx</p>
+    <>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+        <div className="flex flex-row justify-between">
+          <h1 className="text-xl">
+            Editar dados de <span className="font-bold"> {data.name} </span>
+          </h1>
+          <button
+            onClick={closeModal}
+            className="text-white font-bold p-2 bg-red-500 rounded-full"
+          >
+            Fechar
+          </button>
+        </div>
+
+        <div className="flex flex-col mx-auto justify-center p-4 md:w-4/12">
+          <label
+            for="name"
+            className="text-dark_grey text-2xl font-light mt-6 w"
+          >
+            Nome
+          </label>
+          <input
+            className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+            type="text"
+            name="name"
+            value={data.name}
+            onChange={handleChange}
+          />
+          <label for="sex" className="text-dark_grey text-2xl font-light mt-6">
+            Sexo
+          </label>
+          <select
+            name="sex"
+            id="sex"
+            className="w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            value={data.sex}
+            onChange={handleChange}
+          >
+            <option value="masculino">Masculino</option>
+            <option value="feminino">Feminino</option>
+            <option value="aluno">Aluno</option>
+          </select>
+          <label
+            for="neighborhood"
+            className="text-dark_grey text-2xl font-light mt-6"
+          >
+            Bairro
+          </label>
+          <input
+            className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+            type="text"
+            name="neighborhood"
+            placeholder="Digite o bairro da academia"
+            value={data.neighborhood}
+            onChange={handleChange}
+          />
+          <label className="text-dark_grey text-2xl font-light mt-6 w">
+            Rua
+          </label>
+          <input
+            className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+            type="text"
+            name="street"
+            placeholder="Digite a rua da academia"
+            value={data.street}
+            onChange={handleChange}
+          />
+          <label className="text-dark_grey text-2xl font-light mt-6 w">
+            Número
+          </label>
+          <input
+            className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+            type="number"
+            name="number"
+            placeholder="Digite o bairro da academia"
+            value={data.number}
+            onChange={handleChange}
+          />
+          <label className="text-dark_grey text-2xl font-light mt-6 w">
+            CEP
+          </label>
+          <InputMask
+            mask="99999-999"
+            value={data.zipcode}
+            className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none border-gray-600 placeholder-gray-600"
+            name="zipcode"
+            placeholder="Digite o cep da academia"
+            onChange={handleChange}
+          />
+          <label className="text-dark_grey text-2xl font-light mt-6 w">
+            Telefone
+          </label>
+          <InputMask
+            mask="99 99999-9999"
+            name="phone"
+            value={data.phone}
+            className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+            placeholder="Digite o telefone da academia"
+            onChange={handleChange}
+          />
+          <label
+            for="height"
+            className="text-dark_grey text-2xl font-light mt-6 w"
+          >
+            Altura
+          </label>
+          <input
+            className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+            type="text"
+            name="height"
+            placeholder="Digite a altura do aluno. Ex: 169"
+            value={data.height}
+            onChange={handleChange}
+          />
+          <label
+            for="weight"
+            className="text-dark_grey text-2xl font-light mt-6 w"
+          >
+            Peso
+          </label>
+          <input
+            className="border-b border-gray-600 placeholder-gray-600 py-1 text-dark_grey outline-none"
+            type="text"
+            name="weight"
+            placeholder="Digite o peso do aluno. Ex: 169"
+            value={data.weight}
+            onChange={handleChange}
+          />
+          <button
+            type="button"
+            onClick={handleEdit}
+            className="bg-primary mt-8 p-2 rounded-full text-white font-bold w-1/2 hover:opacity-75 focus:outline-none mx-auto"
+          >
+            Cadastrar
+          </button>
+        </div>
+      </Modal>
+      <div className=" flex flex-col md:flex-row border-t p-4">
+        <p className="font-bold text-dark_grey md:w-2/6">{data.name}</p>
+        <div className="flex flex-row md:w-2/6">
+          <Zap className="h-12 w-12 pb-5" />
+          <p className="text-dark_grey md:pl-2">{data.phone}</p>
+        </div>
+        <div className="flex flex-row md:w-1/6">
+          <Regua className="h-12 w-12 pb-5" />
+          <p className="text-dark_grey md:pl-2">{data.height} cm</p>
+        </div>
+        <div className="flex flex-row md:w-1/6">
+          <Peso className="h-12 w-12 pb-5" />
+          <p className="text-dark_grey md:pl-2">{data.weight} kg</p>
+        </div>
+        <div className="flex flex-row md:w-11/12">
+          <Casa className="h-12 w-12 pb-5" />
+          <p className="text-dark_grey md:pl-2">
+            {data.street}, {data.number}, {data.eighborhood}, {data.city} -{' '}
+            {data.zipcode}
+          </p>
+        </div>
+        <div className="flex flex-row md:w-6/6">
+          <Delete
+            onClick={handleDelete}
+            className="h-6 w-6 fill-current text-red-600"
+          />
+          <Edit
+            onClick={openModal}
+            className="h-6 w-6 fill-current ml-4 text-primary"
+          />
+        </div>
       </div>
-      <div className="flex flex-row md:w-1/6">
-        <Regua className="h-12 w-12 pb-5" />
-        <p className="text-dark_grey md:pl-2">175 cm</p>
-      </div>
-      <div className="flex flex-row md:w-1/6">
-        <Peso className="h-12 w-12 pb-5" />
-        <p className="text-dark_grey md:pl-2">110 kg</p>
-      </div>
-      <div className="flex flex-row md:w-5/6">
-        <Casa className="h-12 w-12 pb-5" />
-        <p className="text-dark_grey md:pl-2">
-          Rua Barbosa Ferraz, 272, Centro, Cachoeira Paulista - 12630-000
-        </p>
-      </div>
-      <div className="flex flex-row md:w-6/6">
-        <Delete className="h-6 w-6 fill-current text-red-600" />
-        <Edit className="h-6 w-6 fill-current ml-4 text-primary" />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Students from '../../components/Students/index';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 const Dashboard = () => {
+  const token = localStorage.getItem('token');
+  api.defaults.headers.Authorization = token;
+
+  const getGymData = async () => {
+    const response = await api.post('/api/auth/me');
+    console.log(response);
+  };
+
+  getGymData();
+
+  const getClientData = async () => {
+    const response = await api.get('/api/clients');
+    setClientsData(response.data.clients.data);
+  };
+
+  getClientData();
+
+  const [clientsData, setClientsData] = useState([]);
+
   return (
     <>
       <div className="flex md:px-32 md:py-6 pl-8 py-3">
-        <button className="bg-primary mt-8 px-4 py-2 rounded-full text-white font-bold ">
+        <Link
+          to="/newclient"
+          className="bg-primary mt-8 px-4 py-2 rounded-full text-white font-bold "
+        >
           Adicionar Alunos
-        </button>
+        </Link>
       </div>
       <div className="flex flex-col justify-center p-4 lg:px-40 ">
-        <Students />
-        <Students />
-        <Students />
-        <Students />
-        <Students />
-        <Students />
-        <Students />
-        <Students />
-        <Students />
+        {clientsData.map((student, i) => {
+          return (
+            <Students
+              key={i}
+              id={student.id}
+              name={student.name}
+              phone={student.phone}
+              weight={student.weight}
+              height={student.height}
+              street={student.street}
+              number={student.number}
+              neighborhood={student.neighborhood}
+              city={student.city}
+              zipcode={student.zipcode}
+              sex={student.sex}
+            />
+          );
+        })}
       </div>
     </>
   );
