b0d13fc645ce82a70a953ee54f0ae903bb8217c0
timeoutguy <marcusvyny92@gmail.com>
2020-11-30 13:47:44 -0300

    AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { AuthProvider } from './context/auth';
 
+import Navbar from './components/Navbar';
 import Routes from './routes';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes />
-      <Footer />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes />
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
