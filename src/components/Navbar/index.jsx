9468b28cccb9aff4f06d1a4b21bb362d34ea0b0e
timeoutguy <marcusvyny92@gmail.com>
2020-11-30 13:48:08 -0300

    Logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 const Navbar = () => {
   return (
     <div className="bg-primary h-20 px-2 lg:px-64 flex justify-between items-center font-bold">
       <Link to="/" className="text-3xl text-white">
-        Gym track
+        <Logo className="w-32 h-32" />
       </Link>
       <div className="flex flex-row">
         <Link
