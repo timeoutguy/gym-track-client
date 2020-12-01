c3262a7a632c9dd939c206d3f138433572156d39
timeoutguy <marcusvyny92@gmail.com>
2020-12-01 09:29:56 -0300

    Deixando o componente de instrutores funcional e adicionando página de criação de instrutor

--- src/pages/Instructor/index.jsx
+++ src/pages/Instructor/index.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Instructors from '../../components/Instructors';
 import { Link } from 'react-router-dom';
-import Students from '../../components/Students/index';
+import api from '../../services/api';
 
 const Dashboard = () => {
+  const [instructorsData, setInstructorsData] = useState([]);
+
+  const token = localStorage.getItem('token');
+  api.defaults.headers.Authorization = token;
+
+  useEffect(() => {
+    const getInstructorData = async () => {
+      const response = await api.get('/api/instructors/gym/1');
+      setInstructorsData(response.data.instructors.data);
+    };
+    getInstructorData();
+  }, [instructorsData]);
+
   return (
     <>
       <div className="flex justify-between md:px-32 md:py-6 pl-8 py-3">
         <Link
-          to="/newclient"
+          to="/newinstructor"
           className="bg-primary mt-8 px-4 py-2 rounded-full text-white font-bold "
         >
           Adicionar Instrutor
@@ -18,14 +31,20 @@ const Dashboard = () => {
         </Link>
       </div>
       <div className="flex flex-col justify-center p-4 lg:px-40 ">
-        <Instructors />
-        <Instructors />
-        <Instructors />
-        <Instructors />
-        <Instructors />
-        <Instructors />
-        <Instructors />
-        <Instructors />
+        {instructorsData.map((instructor, i) => {
+          return (
+            <Instructors
+              key={i}
+              id={instructor.id}
+              name={instructor.name}
+              phone={instructor.phone}
+              email={instructor.email}
+              gym_id={instructor.gym_id}
+              arrive={instructor.arrive}
+              leave={instructor.leave}
+            />
+          );
+        })}
       </div>
     </>
   );
